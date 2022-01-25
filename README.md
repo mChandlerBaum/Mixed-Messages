a45d337ad4b48ed3b35b0bd5de8672c156c83f9e
Martin Chandler-Baum <martin.chandlerbaum@gmail.com>
2022-01-25 11:49:16 -0400

    My first commit

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # My first project outside of codecademy
 
-**Intro**
-Mixed Messages will generate a new random message every time the user asks it to.
+### Intro
+Mixed Messages will generate a new random message every time the user asks it to. For now I am writing minimal code to get the functionality and practicing making commits to get a feel for everything.
 
-**Languages**
+### Languages
 This program will use JS and possibly some HTMl/CSS if it's not pretty enough.
 
-**Programming**
+### Programming
 In Mixed Messages I will use arrays of _nouns_, _adjectives_ and _verbs_ to randomly input into multiple pre-written texts. I have not decided on the topic yet. Will update as we go.
 
-**Types of Messages**
-I would like the main theme to be comedy. Let's try to not to get too offensive here.
+### Types of Messages
+I would like the main theme to be comedy. Let's try to not get too offensive here.
 -Astrology
 -Paramedicine
 -Daily life with a dog
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello from Mixed Messages');
+//arrays of signs, seasons and advice for a mixed message on astrology
+let signs = ['Moon', 'Comet', 'Stars', 'Three Sisters', 'Sun'];
+let seasons = ['Spring', 'Summer', 'Fall', 'Winter'];
+let advice = ['eat all of the chips.', 'probably not a good idea to go outside today', 'just do the thing you want to do', 'give your doggo more treats', 'go out, for you will meet the love of your life today'];
+
+
+//select random index from arrays
+let randIdx = (array) => {
+    return Math.floor(Math.random() * array.length);
+}
+
+console.log(randIdx(signs));
+
+//here's where the random selection gets filled into
+let message1 = `Ahh, I see it. Your being watched over by the ${signs[randIdx(signs)]}. I know it is not yet ${seasons[randIdx(seasons)]} but I see something through the haze.. yes you should ${advice[randIdx(advice)]}.`;
+let message2 = `Hmm.. your ${signs[randIdx(signs)]} sign is not a fan of what you did last ${seasons[randIdx(seasons)]}. You should ${advice[randIdx(advice)]}.`
+let message3 = `Oh! Oh oh oh. the ${signs[randIdx(signs)]} LOVED that. It tells me that this ${seasons[randIdx(seasons)]} you should ${advice[randIdx(advice)]}.`
+console.log(message1);
